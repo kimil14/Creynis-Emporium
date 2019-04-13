70a300700f2c6706a863f128c3d940b5a013171e
kimil14 <cestre.benjamin@gmail.com>
2019-04-13 16:48:28 +0200

    Update config.js

--- static/config.js
+++ static/config.js
@@ -2,18 +2,18 @@
 // ID of the Google spreadsheet
 // ***********************************************************************************
 // Make sure your spreadsheet is published (File > Publish to the web…)
-var spreadsheetId = "1YBSb3r7qfCJvaWCXVRHBDA1iDMBzGXQUZC4CklHHEqs";
+var spreadsheetId = "1Zjh40WdNfIUAiFoEN5SwU_ozurNKNQlj7z2mEsIYJWg";
 
 // ***********************************************************************************
 // Your FC and IGN
 // ***********************************************************************************
-var friendCode = "2595-5634-2087";
-var inGameName = "Johnny";
+var friendCode = "3754-7845-5418";
+var inGameName = "ledresseur14";
 
 // ***********************************************************************************
 // URL of your Reddit/Twitter account
 // ***********************************************************************************
-var contactUrl = "Discord: TheJigmeister#4272";
+var contactUrl = "Discord: ledresseur14#8427";
 
 // ***********************************************************************************
 // URL of your PGL trainer icon (optional)
